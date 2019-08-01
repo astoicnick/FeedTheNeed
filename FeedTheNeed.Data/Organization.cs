d3c0d5f7ea65b86515b026001d2bb5eb46d66416
Nick Perry <perry184@purdue.edu>
2019-08-01 16:41:49 -0400

    Finished Data layer

--- FeedTheNeed.Data/Organization.cs
+++ FeedTheNeed.Data/Organization.cs
@@ -16,4 +16,3 @@ namespace FeedTheNeed.Data
         public string OrganizationBio { get; set; }
     }
 }
-}
--- FeedTheNeed.Data/User.cs
+++ FeedTheNeed.Data/User.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FeedTheNeed.Data
 {
-    public class User
+    public class User : IdentityUser
     {
         [Key]
         public int UserID { get; set; }
@@ -20,12 +23,14 @@ namespace FeedTheNeed.Data
         public string Email { get; set; }
 
         public double HelpfulRating { get; set; }
-        
-       
-    
 
-
-
-
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            //What is this doing?
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
     }
 }
